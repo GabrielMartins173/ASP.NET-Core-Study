1dbdc65e785b135c4e2076d97e535721977d5277
Gabriel Martins <gabriel_martins1@dellteam.com>
2020-01-02 14:21:59 -0300

    Implemented status code in all the http request methods.

--- QuotesApi/QuotesApi/Controllers/QuotesController.cs
+++ QuotesApi/QuotesApi/Controllers/QuotesController.cs
@@ -22,45 +22,80 @@ namespace QuotesApi.Controllers
         }
         // GET: api/Quotes
         [HttpGet]
-        public IEnumerable<Quote> Get()
+        public IActionResult Get()
         {
-            return _quotesDbContext.Quotes;
+            //return _quotesDbContext.Quotes;
+             return Ok(_quotesDbContext.Quotes); // this way will return the list with all the quotes and the status 200 ok.
+            // we have the other status codes to return too. For example if we need to return NotFound (404) or BadRequest (400).
+
+            //return StatusCode(StatusCodes.Status200OK); // In this way we can return any type of status code. By number or we can use inside the parenthesis StatusCodes. and the IDE will list to us the codes. 
+
         }
 
         // GET: api/Quotes/5
         [HttpGet("{id}", Name = "Get")]
-        public Quote Get(int id)
+        public IActionResult Get(int id)
         {
            var quote = _quotesDbContext.Quotes.Find(id);
-            return quote;
+
+            if(quote == null)
+            {
+                return NotFound("No record found ...");
+            }
+            else
+            {
+                return Ok(quote);
+            }
+         
         }
 
         // POST: api/Quotes
         [HttpPost]
-        public void Post([FromBody] Quote quote)
+        public IActionResult Post([FromBody] Quote quote)
         {
             _quotesDbContext.Quotes.Add(quote);
             _quotesDbContext.SaveChanges(); //this method saves the modifications on our database. 
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         // PUT: api/Quotes/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Quote quote)
+        public IActionResult Put(int id, [FromBody] Quote quote)
         {
             var entity = _quotesDbContext.Quotes.Find(id); //search at the database for the object that we need to modify. 
-            entity.Title = quote.Title;
-            entity.Author = quote.Author;
-            entity.Description = quote.Description;
-            _quotesDbContext.SaveChanges();
+
+            if(entity == null)
+            {
+                return NotFound("No record found against this id ...");
+            }
+            else
+            {
+                entity.Title = quote.Title;
+                entity.Author = quote.Author;
+                entity.Description = quote.Description;
+                _quotesDbContext.SaveChanges();
+                return Ok("Record updated successfully...");
+            }
+  
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
            var quote = _quotesDbContext.Quotes.Find(id);
-            _quotesDbContext.Quotes.Remove(quote);
-            _quotesDbContext.SaveChanges();
+
+            if(quote == null)
+            {
+                return NotFound("No record found against this id ...");
+            }
+            else
+            {
+                _quotesDbContext.Quotes.Remove(quote);
+                _quotesDbContext.SaveChanges();
+                return Ok("Quote deleted ...");
+            }
+            
         }
     }
 }
